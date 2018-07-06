0a888654a30aab1d27a4a79cc67fd3983aaf01c2
Unknown <przempore@gmail.com>
2018-07-06 21:18:19 +0200

    build uncommented

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 rm example.py example_wrap.cxx *.o *.so
 
-# swig3.0 -c++ -python example.i
-# g++ -fpic -c example.c example_wrap.cxx Word.cpp -I/usr/include/python2.7/
-# gcc -shared example_wrap.o Word.o -o _example.so -lstdc++
+swig3.0 -c++ -python example.i
+g++ -fpic -c example.c example_wrap.cxx Word.cpp -I/usr/include/python2.7/
+gcc -shared example_wrap.o Word.o -o _example.so -lstdc++
