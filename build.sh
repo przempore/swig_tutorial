58b2158bfc47d448e4efb87029d1acf9ee92fa2d
Unknown <przempore@gmail.com>
2018-07-06 21:23:32 +0200

    check if file exists

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm example.py example_wrap.cxx *.o *.so
+if [ -f example.py ]; then
+	rm example.py example_wrap.cxx *.o *.so
+fi
 
 swig3.0 -c++ -python example.i
 g++ -fpic -c example.c example_wrap.cxx Word.cpp -I/usr/include/python2.7/
